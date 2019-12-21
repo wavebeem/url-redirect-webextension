21a19b64417423ee4e9d03145dced254c0330e07
Brian Mock <brian@mockbrian.com>
2019-12-21 15:14:59 -0800

    ok

--- src/configure.js
+++ src/configure.js
@@ -8,6 +8,11 @@ const app = new Vue({
   el: "#app",
 
   data: {
+    css: {
+      input: "border-box db w-100 code ba b--black-20 bw1 br1 mv2 pa2",
+      add: "b db bg-dark-blue white b--transparent ba bw1 br2 mv2 ph3 pv2",
+      remove: "b db dark-red bg-white b--dark-red ba bw1 br2 mv2 ph3 pv2"
+    },
     config: page.getConfig()
   },
 
@@ -40,4 +45,4 @@ app.$watch("configJSON", json => {
   page.updateConfig(json);
 });
 
-Object.assign(globalThis, { app });
+Object.assign(globalThis, { app, page });
