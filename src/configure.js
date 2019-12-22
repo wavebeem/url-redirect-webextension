61a38586d840ead3a51d5a68f9daf9f82a45a3e2
Brian Mock <brian@mockbrian.com>
2019-12-21 18:59:47 -0800

    cool

--- src/configure.js
+++ src/configure.js
@@ -9,9 +9,13 @@ const app = new Vue({
 
   data: {
     css: {
-      input: "border-box db w-100 code ba b--black-20 bw1 br1 mv2 pa2",
-      add: "b db bg-dark-blue white b--transparent ba bw1 br2 mv2 ph3 pv2",
-      remove: "b db dark-red bg-white b--dark-red ba bw1 br2 mv2 ph3 pv2"
+      label: "db b nb2",
+      input:
+        "input-reset button-reset border-box db w-100 code ba b--black-20 bw1 br1 mv3 pa2 Focus-Border",
+      add:
+        "input-reset button-reset b db bg-dark-blue white b--transparent ba bw1 br2 mv3 ph3 pv2 Focus-Border Hover-Dim-75",
+      remove:
+        "input-reset button-reset b db dark-red bg-white b--dark-red ba bw1 br2 mv3 ph3 pv2 Focus-Border Hover-Dim-50"
     },
     config: page.getConfig()
   },
