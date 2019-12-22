a841b05cfdfe35bc51fbbd5f302284d8c0e3401a
Brian Mock <brian@mockbrian.com>
2019-12-22 10:03:35 -0800

    pretty icons

--- src/configure.js
+++ src/configure.js
@@ -10,25 +10,26 @@ const app = new Vue({
   data: {
     css: {
       h2: "normal",
-      th: "tl normal",
+      th: "tl normal pv1",
       card: "flex",
       table: "collapse mv4 dt--fixed",
-      row: "b--black-10 bb bw1",
+      row: "b--black-10 bb",
       label: "db b mt3 nb2",
-      checkbox: "Checkbox",
+      checkbox:
+        "Checkbox Focus-Border w2 h2 br1 ba b--black-20 bg-white input-reset",
       input:
-        "input-reset button-reset f6 border-box db w-100 code ba b--black-20 bw1 br1 mv3 pa2 Focus-Border",
+        "input-reset button-reset f6 border-box db w-100 code ba b--black-20 br1 mv2 pa2 Focus-Border h2",
       add:
-        "input-reset button-reset b db bg-dark-blue white b--transparent ba bw1 br2 mv3 ph3 pv2 Focus-Border Hover-Dim-75",
+        "input-reset button-reset b db bg-moon-gray black b--transparent ba br2 mv2 ph3 pv2 Focus-Border hover-bg-light-silver",
       remove:
-        "input-reset button-reset b db dark-red bg-white b--dark-red ba bw1 br2 mv3 ph3 pv2 Focus-Border Hover-Dim-50"
+        "input-reset button-reset b db bg-transparent ba b--transparent br1 mv2 Focus-Border hover-bg-washed-red w2 h2 Remove"
     },
     config: page.getConfig()
   },
 
   methods: {
     addCSP() {
-      this.config.removeCSP.push({ originPattern: "" });
+      this.config.removeCSP.push({ originPattern: "", enabled: true });
     },
 
     removeCSPByIndex(index) {
@@ -36,7 +37,11 @@ const app = new Vue({
     },
 
     addRedirect() {
-      this.config.redirect.push({ fromPattern: "", toPattern: "" });
+      this.config.redirect.push({
+        fromPattern: "",
+        toPattern: "",
+        enabled: true
+      });
     },
 
     removeRedirectByIndex(index) {
